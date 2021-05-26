6da8ffdaf78fcd82be9dc147108d978ff1d7824e
EllieAzaveda <elliemiller66@icloud.com>
2021-05-26 12:07:33 -0600

    Add test to add and delete reservations

--- cypress/integration/main_page_spec.js
+++ cypress/integration/main_page_spec.js
@@ -28,4 +28,22 @@ describe('Show main page of Turing Cafe App', () => {
       .get('[data-cy=submit-button]').should('be.visible')
   });
 
+  it('Should display all reservations on the main page', () => {
+    cy.get('.card').should('have.length', 1)
+  });
+
+  it('Should be able to add a new reservation to the main page', () => {
+    cy.get('form > [data-cy=name]').type('New name')
+      .get('form > [data-cy=date]').type('10/31')
+      .get('form > [data-cy=time]').type('7:45')
+      .get('form > [data-cy=number]').type('8')
+      .get('form > [data-cy=submit-button]').click()
+      .get('.card').should('have.length', 2)
+  });
+
+  it('Should be able to delete a reservation from the main page', () => {
+    cy.get('.card > button').click()
+      .get('div').find('.card').should('have.length', 0)
+  });
+
 });
