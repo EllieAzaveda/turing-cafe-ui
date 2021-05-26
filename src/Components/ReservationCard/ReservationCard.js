d0b73b8603093cc43c4c74e0677ff81550cd6637
EllieAzaveda <elliemiller66@icloud.com>
2021-05-26 12:08:56 -0600

    Fix button styling

--- src/Components/ReservationCard/ReservationCard.js
+++ src/Components/ReservationCard/ReservationCard.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './ReservationCard.css';
 
-const ReservationCard = ({ name, date, time, number, id }) => {
+const ReservationCard = ({ name, date, time, number, id, deleteReservation }) => {
   return (
-    <div className='card'>
+    <div className='card' style={{ backgroundColor: 'white',  color: '#008080' }}>
       <h3 data-cy='name' className='name'>{name}</h3>
       <p data-cy='date' className='date'>{date}</p>
       <p data-cy='time' className='time'>{time}</p>
       <p data-cy='guests' className='guests'>{`Number of guests: ${number}`}</p>
-      <button>Cancel</button>
+      <button className='delete-button'
+        style={{ color: '#CCCCCC', backgroundColor: '#008080', width: '12rem' }}
+        onClick={() => deleteReservation(id)}>Cancel</button>
     </div>
   )
 }
